ba8da2535b8f25c45cbeed44bcf2fdfd6dd772eb
Taisei Yokoshima <taiseiyokoshima030712@gmail.com>
2025-05-20 17:12:09 +0100

    postgres pool connection is working

--- backend/src/database/init.rs
+++ backend/src/database/init.rs
@@ -3,10 +3,11 @@ use diesel_async::pooled_connection::AsyncDieselConnectionManager;
 use diesel_async::AsyncPgConnection;
 
 
+const postgres_string: &str = "postgres://postgres:postgres@127.0.0.1:5432/tkl-chat";
 
 pub async fn initi_pool() -> Result<Pool<AsyncPgConnection>, BuildError> {
 
-    let pool_config = AsyncDieselConnectionManager::<AsyncPgConnection>::new( "test");
+    let pool_config = AsyncDieselConnectionManager::<AsyncPgConnection>::new(postgres_string);
     let pool_result = Pool::builder(pool_config).build(); 
 
 
--- backend/src/server.rs
+++ backend/src/server.rs
@@ -1,7 +1,14 @@
-use backend::database;
+use backend::database::init::initi_pool;
 
 #[tokio::main]
 async fn main() {
+    let result = initi_pool().await;
+
+
+    match result {
+        Err(e) => eprintln!("{}", e),
+        Ok(_) => println!("connected"),
+    }
     
 
 }
