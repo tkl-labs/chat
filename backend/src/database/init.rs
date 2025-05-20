09a321ba8d8f15a2129e01d4b56eff40227e3263
Taisei Yokoshima <taiseiyokoshima030712@gmail.com>
2025-05-20 20:53:24 +0100

    now init-pools eagerly make connections

--- backend/src/database/init.rs
+++ backend/src/database/init.rs
@@ -1,22 +1,38 @@
 use diesel_async::AsyncPgConnection;
 use diesel_async::pooled_connection::AsyncDieselConnectionManager;
-use diesel_async::pooled_connection::deadpool::{BuildError, Pool};
+use diesel_async::pooled_connection::deadpool::{BuildError, Object, Pool};
 use dotenv::dotenv;
-use std::env;
+use std::{env, usize};
 
-pub async fn init_pool() -> Result<Pool<AsyncPgConnection>, BuildError> {
-    dotenv().ok(); // load .env
+pub async fn init_pool(max_size: usize) -> Result<Pool<AsyncPgConnection>, BuildError> {
+    dotenv().ok(); 
 
     let database_url =
         env::var("DATABASE_URL").expect("ERROR: DATABASE_URL must be present in '.env'");
 
     let pool_config = AsyncDieselConnectionManager::<AsyncPgConnection>::new(&database_url);
-    let pool_result = Pool::builder(pool_config).build();
+    let pool_result = Pool::builder(pool_config).max_size(max_size).build();
 
     let pool = match pool_result {
         Err(e) => return Err(e),
         Ok(value) => value,
     };
+
+
+    let mut connections = Vec::<Object<AsyncPgConnection>>::new();
+
+
+    for _ in 0..max_size {
+        let connection = pool.get().await;
+
+        match connection {
+            Err(_) => panic!("could not connect"),
+            Ok(con ) => connections.push(con),
+        };
+    };
+
+    drop(connections);
+
 
     return Ok(pool);
 }
--- backend/src/server.rs
+++ backend/src/server.rs
@@ -2,7 +2,7 @@ use backend::database::init::init_pool;
 
 #[tokio::main]
 async fn main() {
-    let result = init_pool().await;
+    let result = init_pool(5).await;
 
     match result {
         Err(e) => eprintln!("{}", e),
