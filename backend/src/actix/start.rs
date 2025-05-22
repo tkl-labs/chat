bd534bf8f55916d1ea7b43a8ade683d28d80e4db
Lewis Rye <lewisryework@gmail.com>
2025-05-22 15:23:35 +0100

    Bug fixes for Docker Compose

--- backend/src/actix/start.rs
+++ backend/src/actix/start.rs
@@ -1,13 +1,15 @@
 use actix_web::{App, HttpServer};
 use super::routes::apply_routes;
 
+const HTTP_SERVER_URL: &str = "127.0.0.1";
+const HTTP_SERVER_PORT: u16 = 8080;
 
 pub async fn start_server() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
             .configure(apply_routes)
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind((HTTP_SERVER_URL, HTTP_SERVER_PORT))?
         .run()
         .await
 
