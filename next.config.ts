a676ba5c7aeab1781f3f5b65e650e82d21add686
Lewis Rye <lewisryework@gmail.com>
2025-06-01 22:50:45 +0100

    Fix `npm run tauri build`

--- next.config.ts
+++ next.config.ts
@@ -7,7 +7,7 @@ const internalHost = process.env.TAURI_DEV_HOST || "127.0.0.1";
 const nextConfig: NextConfig = {
   // Ensure Next.js uses SSG instead of SSR
   // https://nextjs.org/docs/pages/building-your-application/deploying/static-exports
-  // output: "export",
+  output: "export",
   // Note: This feature is required to use the Next.js Image component in SSG mode.
   // See https://nextjs.org/docs/messages/export-image-api for different workarounds.
   images: {
